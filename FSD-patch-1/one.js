ad5c0bcaaf6a8cb679e8cd244a3d3b5616a7d89a
AmareshMuddebihal <amareshsm123@gmail.com>
2024-02-06 17:39:20 +0530

    Added FSD-Patch-1
     ,

---
+++ FSD-patch-1/one.js
@@ -0,0 +1,62 @@
+let isError = false;
+
+        function backspace() {
+            let display = document.getElementById("display");
+            display.value = display.value.slice(0, -1);
+            isError = false;
+        }
+
+        function calculate() {
+            let display = document.getElementById("display");
+            let expression = display.value;
+            let result;
+
+            try {
+                // Convert trigonometric function inputs from degrees to radians
+                expression = expression.replace(/sin\(/g, 'sin(' + Math.PI / 180 + '*');
+                expression = expression.replace(/cos\(/g, 'cos(' + Math.PI / 180 + '*');
+                expression = expression.replace(/tan\(/g, 'tan(' + Math.PI / 180 + '*');
+
+                result = math.evaluate(expression);
+                display.value = result;
+                isError = false;
+            } catch (error) {
+                display.value = "Error";
+                isError = true;
+            }
+        }
+
+        function squareRoot() {
+            let display = document.getElementById("display");
+            if (!isError) {
+                display.value += "sqrt(";
+            }
+        }
+
+        function base10Log() {
+            let display = document.getElementById("display");
+            if (!isError) {
+                display.value += "log(";
+            }
+        }
+
+        function pi() {
+            let display = document.getElementById("display");
+            if (!isError) {
+                display.value += "pi";
+            }
+        }
+
+        function e() {
+            let display = document.getElementById("display");
+            if (!isError) {
+                display.value += "e";
+            }
+        }
+
+        function power() {
+            let display = document.getElementById("display");
+            if (!isError) {
+                display.value += "^(";
+            }
+        }
