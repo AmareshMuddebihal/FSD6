5a7954b4daa3e8d3f9610cb0660dfbfaa11d7c90
AmareshSM <87269971+AmareshMuddebihal@users.noreply.github.com>
2024-02-20 10:14:58 +0530

    Add files via upload

---
+++ validationpages/action_page.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>repl.it</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    
+    <script src="script.js"></script>
+    <form action="action_page.php" target="_self" method="post">
+    Username: <br><input type="text" name="UsrNme"><br>
+    <input type="submit" value="Play">
+
+    
+    </form>
+  </body>
+</html>
